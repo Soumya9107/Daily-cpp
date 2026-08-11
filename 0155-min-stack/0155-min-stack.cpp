7b03b9dbb9c02bfcd29b215d95225ba41ca093c1
Soumyajit Maity <soumyjitmaity31@gmail.com>
2026-08-11 21:15:49 +0530

    Time: 51 ms (78.67%), Space: 152.7 MB (6.49%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,37 @@
+class MinStack {
+private:
+    vector<int> myStack;
+    vector<int> minStack;
+    
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        myStack.push_back(val);
+        if(minStack.empty()){ minStack.push_back(val); }
+        else{
+            val = min(val, minStack.back());
+            minStack.push_back(val);
+        }
+    }
+    
+    void pop() {
+        if(myStack.empty() && minStack.empty()){
+            return;
+        }
+        myStack.pop_back();
+        minStack.pop_back();
+    }
+    
+    int top() {
+        if(myStack.empty()){ return -1; }
+        return myStack.back();
+    }
+    
+    int getMin() {
+        if(minStack.empty()){ return -1; }
+        return minStack.back();
+    }
+};
